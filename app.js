d77e7e44660421e4618b289471730a18892e397d
Purnima Sharma <57852378+purnima143@users.noreply.github.com>
2023-01-15 01:57:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
+const {MONGOURI} = process.env.MONGOURI
 
 
 mongoose.connect(MONGOURI,{
